a12519c92900beedda5b5879768acf3cfadf8f9d
HyoseopSong <csnvshs@naver.com>
2017-08-15 16:50:51 +0900

    매장 신규 등록 과정 추가  한글화

--- westgateproject/View/Register.xaml.cs
+++ westgateproject/View/Register.xaml.cs
@@ -46,27 +46,34 @@ namespace westgateproject.View
         }
 		private async void RegisterClicked(object sender, EventArgs e)
 		{
-
-            IDictionary<string, string> postDictionary = new Dictionary<string, string>
+            if(shopNameEntry.Text == null || phoneNumberEntry.Text == null)
             {
-                { "id", App.userEmail},
-                { "name", shopNameEntry.Text },
-				{ "building", _building},
-				{ "floor", _floor},
-				{ "location", _location },
-				{ "number", phoneNumberEntry.Text},
-                { "addInfo", addInfo.Text}
-            };
-            await App.Client.InvokeApiAsync("userInformation", System.Net.Http.HttpMethod.Post, postDictionary);
+                await DisplayAlert("빈 칸 있음", "매장 이름과 전화 번호를 입력해 주세요.", "확인");
+            }
+            else
+            {
+				IDictionary<string, string> postDictionary = new Dictionary<string, string>
+					{
+					{ "id", App.userEmail},
+					{ "name", shopNameEntry.Text },
+					{ "building", _building},
+					{ "floor", _floor},
+					{ "location", _location },
+					{ "number", phoneNumberEntry.Text},
+					{ "addInfo", addInfo.Text}
+				};
+				await App.Client.InvokeApiAsync("userInformation", System.Net.Http.HttpMethod.Post, postDictionary);
 
-			await DisplayAlert("Register Reqeust Completed!", "After you give me the money, it will be done within this midnight", "Done");
+				await DisplayAlert("신청 완료", "내 매장으로 등록 신청이 완료 되었습니다.", "확인");
 
-			await Navigation.PopAsync();
+				await Navigation.PopAsync();
+            }
+
 		}
 
 		private async void CancelClicked(object sender, EventArgs e)
 		{
-			var answer = await DisplayAlert("Request will be deleted!", "Do you want to cancel a request?", "Yes", "No");
+			var answer = await DisplayAlert("취소", "등록 요청을 취소하시겠습니까?", "취소", "무시");
             if (answer)
             {
                 await SyncData.DeleteUserInfo(App.userEmail, _building, _floor, _location);
